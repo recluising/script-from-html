f7b8a67e30a37270d55f433b130a7830e5235022
Luis Recio <recluising@gmail.com>
2017-02-15 22:35:17 +0100

    feat(bin/index.js, package.json): updating deps, using nightmare to iterate nodes

--- bin/index.js
+++ bin/index.js
@@ -1,37 +1,72 @@
 #!/usr/bin/env node
 
-/**
- * Module dependencies.
- */
-
 var program = require('commander');
-var scrapeIt = require('scrape-it');
+var Nightmare = require('nightmare');
 
 program
 	.version('0.1.0')
 	.parse(process.argv);
 
-scrapeIt('http://shakespeare.mit.edu/hamlet/full.html', {
-	chapters: {
-		listItem: 'h3'
-	},
-	narratorFromChapters: {
-		listItem: 'blockquote > i'
-	},
-	messages: {
-		listItem: 'blockquote',
-		data: {
-			fromRole: {
-				selector: 'a'
-			},
-			fromNarrator: {
-				selector: 'p i'
+var nightmare = Nightmare();
+
+nightmare
+	.goto('http://shakespeare.mit.edu/hamlet/full.html')
+	.evaluate(function () {
+		var script = [];
+		var text;
+		var tag;
+
+		var currentUser = 'Director';
+
+		function parseNode(tag, text) {
+			var tagName = tag.toLowerCase();
+			var userName;
+
+			switch (tagName) {
+				case 'h3':
+					currentUser = 'Director';
+					pushScriptLine(currentUser, text)
+					break;
+
+				case 'blockquote':
+					// iterate blockquote when <p><i> is narrator and <a> is conversation
+					break;
+
+				case 'a':
+					userName = //get child <b> content
+					currentUser = userName;
+					break;
 			}
 		}
-	},
-	roles: {
-		listItem: 'a > b'
-	}
-}).then(page => {
-	console.log(page.messages);
-});
+
+		function pushScriptLine(currentUser, message) {
+			script.push({
+				user: currentUser,
+				message: message
+			});
+		}
+
+		var currentNode = document.querySelector('h3');
+
+		// Handling first node
+		script.push(pushScriptLine(currentUser, currentNode.textContent));
+
+		while (currentNode = currentNode.nextElementSibling) {
+			text = currentNode.textContent;
+			if (text) {
+				tag = currentNode.tagName;
+				parseNode(tag, text);
+			}
+		}
+		return script;
+
+	})
+	.end()
+	.then(function (result) {
+		console.log(result);
+	})
+	.catch(function (error) {
+		console.error('Search failed:', error);
+	});
+
+// Test with cheerio and request.
